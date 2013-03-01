4929070db1b3d776c0e63154de40c8e44d5ed3c9
Derrick Weis <derrick@derrickweis.com>
2013-02-28 22:11:32 -0500

    remove blank spaces at end of lines, fix update call

--- public/javascripts/MyApp.js
+++ public/javascripts/MyApp.js
@@ -1,9 +1,9 @@
 MyApp = function( veroldApp ) {
 
-  this.veroldApp = veroldApp;  
+  this.veroldApp = veroldApp;
   this.mainScene;
   this.camera;
-  
+
   this.socket = io.connect('http://localhost');
 }
 
@@ -12,7 +12,7 @@ MyApp.prototype.startup = function( ) {
   var that = this;
 
 	this.veroldApp.loadScene( null, {
-    
+
     success_hierarchy: function( scene ) {
       that.socket.on('update', function(updateObj) {
         console.log(updateObj);
@@ -24,19 +24,18 @@ MyApp.prototype.startup = function( ) {
       that.inputHandler = that.veroldApp.getInputHandler();
       that.renderer = that.veroldApp.getRenderer();
       that.picker = that.veroldApp.getPicker();
-      
+
       //Bind to input events to control the camera
       that.veroldApp.on("keyDown", that.onKeyPress, that);
       that.veroldApp.on("mouseUp", that.onMouseUp, that);
-
-      that.veroldApp.on("update", this.update, this );
+      that.veroldApp.on("update", that.update, that );
 
       //Store a pointer to the scene
       that.mainScene = scene;
-      
+
       var models = that.mainScene.getAllObjects( { "filter" :{ "model" : true }});
       var model = models[ _.keys( models )[0] ].threeData;
-      
+
       //Create the camera
       that.camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.1, 10000 );
       that.camera.up.set( 0, 1, 0 );
@@ -49,7 +48,7 @@ MyApp.prototype.startup = function( ) {
       lookAt.add( model.position );
 
       that.camera.lookAt( lookAt );
-      
+
       //Tell the engine to use this camera when rendering the scene.
       that.veroldApp.setActiveCamera( that.camera );
 
@@ -57,21 +56,21 @@ MyApp.prototype.startup = function( ) {
 
     progress: function(sceneObj) {
       var percent = Math.floor((sceneObj.loadingProgress.loaded_hierarchy / sceneObj.loadingProgress.total_hierarchy)*100);
-      that.veroldApp.setLoadingProgress(percent); 
+      that.veroldApp.setLoadingProgress(percent);
     }
   });
-	
+
 }
 
 MyApp.prototype.shutdown = function() {
-	
+
   this.veroldApp.off("keyDown", this.onKeyPress, this);
   this.veroldApp.off("mouseUp", this.onMouseUp, this);
 
   this.veroldApp.off("update", this.update, this );
 }
 
-  
+
 
 MyApp.prototype.update = function( delta ) {
 
@@ -79,12 +78,12 @@ MyApp.prototype.update = function( delta ) {
 
 
 MyApp.prototype.onMouseUp = function( event ) {
-  
 
 
-  if ( event.button == this.inputHandler.mouseButtons[ "left" ] && 
+
+  if ( event.button == this.inputHandler.mouseButtons[ "left" ] &&
     !this.inputHandler.mouseDragStatePrevious[ event.button ] ) {
-    
+
     var mouseX = event.sceneX / this.veroldApp.getRenderWidth();
     var mouseY = event.sceneY / this.veroldApp.getRenderHeight();
     var pickData = this.picker.pick( this.mainScene.threeData, this.camera, mouseX, mouseY );
@@ -98,19 +97,19 @@ MyApp.prototype.onMouseUp = function( event ) {
 }
 
 MyApp.prototype.onKeyPress = function( event ) {
-	
+
 	var keyCodes = this.inputHandler.keyCodes;
   if ( event.keyCode === keyCodes['B'] ) {
     var that = this;
     this.boundingBoxesOn = !this.boundingBoxesOn;
     var scene = veroldApp.getActiveScene();
-    
+
     scene.traverse( function( obj ) {
       if ( obj.isBB ) {
         obj.visible = that.boundingBoxesOn;
       }
     });
-  
+
   }
-    
+
 }
