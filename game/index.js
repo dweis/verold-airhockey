c048e5e7c96df4fec7babc4c4e5dcc70deb959a7
Derrick Weis <derrick@derrickweis.com>
2013-03-03 10:15:14 -0500

    refactoring, add some log messages

--- game/index.js
+++ game/index.js
@@ -80,13 +80,22 @@ Game.prototype.addPlayer = function(player) {
   }
 }
 
+Game.prototype.spectatorRemoved = function(player) {
+  this.io.sockets.emit('spectatorRemove', {
+        name: player.name
+      , gravatarHash: player.gravatarHash
+      , uuid: player.uuid });
+}
+
 Game.prototype.removePlayer = function(player) {
+  console.log(player.name + ' has left the game');
   if (this.p1 && this.p1.socket && this.p1.socket.id == player.socket.id) {
     this.p1 = undefined;
 
     if (this.spectators.length) {
       this.setP1(this.spectators.pop());
-      this.io.sockets.emit('spectatorRemove', { name: this.p1.name, gravatarHash: this.p1.gravatarHash, uuid: this.p1.uuid });
+      this.spectatorRemoved(this.p1);
+
     } else {
       this.io.sockets.emit('p1');
     }
@@ -95,17 +104,14 @@ Game.prototype.removePlayer = function(player) {
 
     if (this.spectators.length) {
       this.setP2(this.spectators.pop());
-      this.io.sockets.emit('spectatorRemove', { name: this.p2.name, gravatarHash: this.p2.gravatarHash, uuid: this.p2.uuid });
+      this.spectatorRemoved(this.p2);
     } else {
       this.io.sockets.emit('p2');
     }
   } else {
     for (var i in this.spectators) {
       if (this.spectators[i].socket.id == player.socket.id) {
-        this.io.sockets.emit('spectatorRemove', {
-          name: this.spectators[i].name,
-          gravatarHash: this.spectators[i].gravatarHash,
-          uuid: this.spectators[i].uuid });
+        this.spectatorRemoved(this.spectators[i]);
         this.spectators.splice(i, 1);
       }
     }
@@ -126,6 +132,8 @@ Game.prototype.setP1 = function(player) {
   this.p1.socket.emit('active', { player: 'p1' });
 
   this.io.sockets.emit('p1', { name: player.name, gravatarHash: player.gravatarHash, uuid: player.uuid });
+
+  console.log(this.p1.name + ' has become player 1');
 }
 
 Game.prototype.setP2 = function(player) {
@@ -136,6 +144,8 @@ Game.prototype.setP2 = function(player) {
   this.p2.socket.emit('active', { player: 'p2' });
 
   this.io.sockets.emit('p2', { name: player.name, gravatarHash: player.gravatarHash, uuid: player.uuid });
+
+  console.log(this.p2.name + ' has become player 2');
 }
 
 Game.prototype.updatePositionP1 = function() {
